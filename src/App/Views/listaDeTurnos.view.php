3dba91e5f3e4973dd6c352ccfc96987879e858fc
Lucas RK <lucasrk00@gmail.com>
2021-05-23 23:22:43 -0300

    lista de turnos sin turnos 🤩

--- src/App/Views/listaDeTurnos.view.php
+++ src/App/Views/listaDeTurnos.view.php
@@ -16,50 +16,57 @@
 	<main>
 		<section>
 			<h2>Turnos de <?= $_SESSION["nombreApellido"] ?></h2>
-			<p>Mostrando turnos filtrados por:</p>
 			<!-- Lista de filtros -->
-			<ul>
-				<li>Fecha del Turno: 17/04/2021 al 30/04/2021</li>
-			</ul>
-
-			<!-- Botón que muestra los filtros -->
-			<button>Filtros</button>
-			<?php
-			require 'parts/statusResultMessage.view.php'
-			?>
-			<table>
-				<tr>
-					<th>Fecha</th>
-					<th>Hora</th>
-					<th>Profesional</th>
-					<th>Especialidad</th>
-					<th>Estado</th>
-					<th></th>
-				</tr>
-				<?php foreach ($turnos as $turno) : ?>
-					<!-- Primer Turno -->
+			<?php if(isset($turnos) && count($turnos)) : ?>
+				<p>Mostrando turnos filtrados por:</p>
+				<ul>
+					<li>Fecha del Turno: 17/04/2021 al 30/04/2021</li>
+				</ul>
+	
+				<!-- Botón que muestra los filtros -->
+				<button>Filtros</button>
+				<?php
+				require 'parts/statusResultMessage.view.php'
+				?>
+				<table>
 					<tr>
-						<td><?= date('Y-m-d', strtotime($turno->fechaHora)) ?></td>
-						<td><?= date('H:m', strtotime($turno->fechaHora)) ?></td>
-						<td><?= $turno->profesional->nombre ?></td>
-						<td><?= $turno->especialidad->nombre ?></td>
-						<?php if ($turno->cancelado) : ?>
-							<td>Cancelado</td>
-						<?php else : ?>
-							<td><?= ($turno->fechaHora < date("Y-m-d H:m")) ? "Finalizado" : "Pendiente" ?></td>
-						<?php endif; ?>
-						<td>
-							<?php if ($turno->cancelado || $turno->fechaHora < date("Y-m-d H:m")) : ?>
-								<button disabled>Cancelar</button>
-							<?php else : ?>
-								<form action="/cancelarTurno?turno=<?= $turno->id ?>" method="post">
-									<button class="danger">Cancelar</button>
-								</form>
-							<?php endif; ?>
-						</td>
+						<th>Fecha</th>
+						<th>Hora</th>
+						<th>Profesional</th>
+						<th>Especialidad</th>
+						<th>Estado</th>
+						<th></th>
 					</tr>
-				<?php endforeach; ?>
-			</table>
+					<?php foreach ($turnos as $turno) : ?>
+						<!-- Primer Turno -->
+						<tr>
+							<td><?= date('Y-m-d', strtotime($turno->fechaHora)) ?></td>
+							<td><?= date('H:m', strtotime($turno->fechaHora)) ?></td>
+							<td><?= $turno->profesional->nombre ?></td>
+							<td><?= $turno->especialidad->nombre ?></td>
+							<?php if ($turno->cancelado) : ?>
+								<td>Cancelado</td>
+							<?php else : ?>
+								<td><?= ($turno->fechaHora < date("Y-m-d H:m")) ? "Finalizado" : "Pendiente" ?></td>
+							<?php endif; ?>
+							<td>
+								<?php if ($turno->cancelado || $turno->fechaHora < date("Y-m-d H:m")) : ?>
+									<button disabled>Cancelar</button>
+								<?php else : ?>
+									<form action="/cancelarTurno?turno=<?= $turno->id ?>" method="post">
+										<button class="danger">Cancelar</button>
+									</form>
+								<?php endif; ?>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+				</table>
+			<?php else: ?>
+				<section class="sin-turnos">
+					<p>No tienes ningún turno</p>
+					<a class="button secondary" href="/solicitarTurno">Solicitar turno</a>
+				</section>
+			<?php endif ?>
 		</section>
 	</main>
 	<!-- Footer del sitio -->
