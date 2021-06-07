e03921f98ef5aa12051404f502d7df4d80e22ad6
Facundo Silva <silva.facundo13@gmail.com>
2021-06-06 22:20:46 -0300

    improve carousel
    Co-authored-by: lucasrk00 <lucasrk00@users.noreply.github.com>

--- public/assets/js/components/ImageCarousel.js
+++ public/assets/js/components/ImageCarousel.js
@@ -1,60 +1,47 @@
 class ImageCarousel {
-	constructor(parent, imagesUrl) {
+	constructor(parent, imagesUrl, transition, interval = 5000) {
 		if (typeof parent === 'string')
 			parent = document.querySelector(parent);
 		if (!parent) throw new Error('Invalid parent');
 		if (!imagesUrl || !Array.isArray(imagesUrl) || imagesUrl.length < 1) throw new Error('Invalid images');
+
 		// Initialize properties
 		this.touchStartX;
 		this.touchEndX;
 		this.slideIndex = 1;
-		const transitions = ['fade', 'show'];
+		this.loadingProgress = 0;
+		this.images = imagesUrl;
+		this.interval = interval;
+		this.selectedTransition = transition;
 
+		// Create loading
+		const loadingOverlay = this.createLoadingOverlay();
+
+		// Create images
 		const imagesContainer = [];
 		const dots = [];
 		for (const [imageIndex, imageUrl] of imagesUrl.entries()) {
-			const image = PAW.createElement('img', 'imagen', {
-				src: imageUrl,
-				style: 'width: 100%',
-			})
-			const transition = transitions[Math.floor(Math.random() * (transitions.length))];
-			const imageContainer = PAW.createElement('div', image, {
-				class: `mySlides ${transition}`
-			});
-			const dot = PAW.createElement('span', '', {
+			const dot = PAW.createElement('li', '', {
 				class: 'dot',
 			})
 			dot.addEventListener('click', () => {
-				this.currentSlide(imageIndex + 1);
+				this.setSlide(imageIndex + 1);
 			})
 			dots.push(dot);
-			imagesContainer.push(imageContainer);
+			imagesContainer.push(this.createImage(imageUrl));
 		}
 
-		const dotsContainer = PAW.createElement('div', dots, {
-			style: 'text-align: center;'
-		});
+		const dotsContainer = PAW.createElement('ul', dots);
 
 		// Create next and previous "buttons"
-		const prevButton = PAW.createElement('a', '<', {
-			class: 'prev-button'
-		});
-		const nextButton = PAW.createElement('a', '>', {
-			class: 'next-button'
-		});
-		prevButton.addEventListener('click', () => {
-			this.plusSlides(-1);
-		});
-		nextButton.addEventListener('click', () => {
-			this.plusSlides(1);
-		});
+		const buttons = this.createButtons();
 
 		// Create image carousel section
-		const carouselSection = PAW.createElement('section', [...imagesContainer, prevButton, nextButton, dotsContainer], {
+		const carouselSection = PAW.createElement('section', [loadingOverlay, ...imagesContainer, ...buttons, dotsContainer], {
 			class: 'image-slider'
 		});
 
-		carouselSection.addEventListener('touchstart', this.setTouchStartX.bind(this));		
+		carouselSection.addEventListener('touchstart', this.setTouchStartX.bind(this));
 		carouselSection.addEventListener('touchend', this.setTouchEndX.bind(this));
 		
 		document.addEventListener('keydown', this.checkKeyPressed.bind(this));
@@ -63,14 +50,65 @@ class ImageCarousel {
 
 		this.element = carouselSection;
 		this.parent = parent;
-		this.showSlides(this.slideIndex);
+		this.setSlide(this.slideIndex);
+	}
+	set progress(val) {
+		if (this.loadingProgress > this.images.length) return;
+		this.loadingProgress = val;
+		this.handleProgress();
+	}
+	get progress() {
+		return this.loadingProgress;
+	}
+	get transitions() {
+		return ['fade', 'show', 'translate'];
 	}
 
-	plusSlides(n) {
-		this.showSlides(this.slideIndex += n);
+	createButtons() {
+		const prevButton = PAW.createElement('a', '<', {
+			class: 'prev-button'
+		});
+		const nextButton = PAW.createElement('a', '>', {
+			class: 'next-button'
+		});
+		prevButton.addEventListener('click', () => {
+			this.nextSlide(-1);
+		});
+		nextButton.addEventListener('click', () => {
+			this.nextSlide(1);
+		});
+		return [prevButton, nextButton];
 	}
-	currentSlide(n) {
-		this.showSlides(this.slideIndex = n);
+	createLoadingOverlay() {
+		const loadingPercentage = PAW.createElement('p', '0%');
+		const loadingText = PAW.createElement('p', 'Cargando');
+		const loadingOverlay = PAW.createElement('div', [loadingText, loadingPercentage], { class:'loading' });
+		this.loading = {
+			text: loadingPercentage,
+			overlay: loadingOverlay
+		}
+		return loadingOverlay;
+	}
+	handleProgress() {
+		if (this.progress >= this.images.length) {
+			this.element.removeChild(this.loading.overlay);
+		} else {
+			this.loading.text.innerText = `${Math.round((this.progress / this.images.length) * 100)}%`;
+		}
+	}
+
+	createImage(url) {
+			const image = PAW.createElement('img', 'imagen', {
+				src: url,
+				style: 'width: 100%',
+			})
+			const transition = this.selectedTransition || this.transitions[Math.floor(Math.random() * (this.transitions.length))];
+			const imageContainer = PAW.createElement('div', image, {
+				class: `slide ${transition}`
+			});
+			image.addEventListener('load', e => this.progress++);
+
+			return imageContainer;
 	}
 
 	setTouchStartX(e) {
@@ -82,33 +120,43 @@ class ImageCarousel {
 	}
 
 	handleGesture() {
-		if (this.touchEndX < this.touchStartX) this.plusSlides(1);
-		if (this.touchEndX > this.touchStartX) this.plusSlides(-1);
+		if (this.touchEndX < this.touchStartX) this.nextSlide();
+		if (this.touchEndX > this.touchStartX) this.nextSlide(-1);
 	}
 
 	checkKeyPressed(e) {
 		switch (e.keyCode) {
 			case 37:
-				this.plusSlides(-1);
+				this.nextSlide(-1);
 				break;
 			case 39:
-				this.plusSlides(1);
+				this.nextSlide(1);
 				break;
 		}
 	}
 
-	showSlides(n) {
-		let slides = document.getElementsByClassName("mySlides");
+	nextSlide(n = 1) {
+		this.setSlide(this.slideIndex += n);
+	}
+	setSlide(n) {
+		this.slideIndex = n;
+		let slides = document.getElementsByClassName("slide");
 		let dots = document.getElementsByClassName("dot");
+
 		if (n > slides.length) {this.slideIndex = 1}
 		if (n < 1) {this.slideIndex = slides.length}
+
 		for (const slide of slides) {
 			slide.style.display = "none";
 		}
 		for (const dot of dots) {
 			dot.className = dot.className.replace(" active-dot", "");
 		}
+
 		slides[this.slideIndex-1].style.display = "block";
 		dots[this.slideIndex-1].className += " active-dot";
+
+		if (this.nextTimeout) clearTimeout(this.nextTimeout);
+		this.nextTimeout = setTimeout(() => this.nextSlide(), this.interval);
 	}
 }
