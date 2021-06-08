9a5677013d0b00f014650fa87eff220a97e2c2ab
Lucas RK <lucasrk00@gmail.com>
2021-06-07 23:02:16 -0300

    improvement client appointmet 🦥

--- public/assets/js/components/ClientAppointment.js
+++ public/assets/js/components/ClientAppointment.js
@@ -12,16 +12,19 @@ class ClientAppointment {
 
 		const title = PAW.createElement('h2', 'Tu turno:' );
 		this.clientAppointmentElement = PAW.createElement('h3', this.clientAppointment);
+		this.clientAppointmentNotifyElement = PAW.createElement('h3', 'Aún no es tu turno');
 
 		this.attendingTitle = PAW.createElement('h2', 'Turnos siendo atendidos: ');
-		this.appointmentSection = PAW.createElement('section', [title, this.clientAppointmentElement, this.soundEffect]);
+		this.appointmentSection = PAW.createElement('section', [title, this.clientAppointmentElement, this.clientAppointmentNotifyElement, this.soundEffect]);
 		this.attendingAppointmentSection = PAW.createElement('section', this.attendingTitle);
 		parent.appendChild(this.appointmentSection);
 		parent.appendChild(this.attendingAppointmentSection);
 	}
 
 	setClientAppointment(appointment){
+		if (this.clientAppointment && this.clientAppointment.id === appointment.id) return;
 		this.clientAppointmentElement.textContent = appointment.id;
+		this.clientAppointmentNotifyElement.textContent = 'Aún no es tu turno';
 		this.clientAppointment = appointment;
 	}
 
@@ -33,10 +36,11 @@ class ClientAppointment {
 			for (const appointment of this.professionalsAppointments[professionalIndex].appointments) {
 				if (appointment.state !== 'attending') continue;
 				attendingAppointments.push(appointment);
-				if (this.clientAppointment && appointment.id === this.clientAppointment.id) {
+				if (this.clientAppointment && appointment.id === this.clientAppointment.id && !this.clientAppointment.alreadyNotified) {
+					this.clientAppointment.alreadyNotified = true;
+					this.clientAppointmentNotifyElement.textContent = 'Es tu turno!!';
 					this.playSound();
 					this.phoneVibrate();
-					this.clientAppointment = '';
 				}
 			}
 		}
